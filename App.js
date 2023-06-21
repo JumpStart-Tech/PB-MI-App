1056cb3d8880076b4b308ad431cff2dd1ff163a4
Grace Labore <grace.c.labore@vanderbilt.edu>
2023-06-21 18:03:59 -0400

    GL first attempt

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Onboarding from './Onboarding';
+import Registration from './Registration';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Onboarding />
+      <Registration />
       <StatusBar style="auto" />
     </View>
   );

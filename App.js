36af28bae8b0d09f23ae6af3708258a97b1315cc
Grace Labore <grace.c.labore@vanderbilt.edu>
2023-06-27 11:27:42 -0400

    Initial commit

--- App.js
+++ App.js
@@ -2,14 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Onboarding from './Onboarding';
-import Registration from './Registration';
-
 export default function App() {
   return (
     <View style={styles.container}>
-      <Onboarding />
-      <Registration />
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );

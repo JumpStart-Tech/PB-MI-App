ebb2b69634879b6bc21d6456e8b29635c2d9a8c9
Jack <jack.k.garritano@vanderbilt.edu>
2023-07-01 13:36:39 -0500

    updated forgot password screen to use the text input component

--- views/ForgotPassword.js
+++ views/ForgotPassword.js
@@ -3,28 +3,28 @@ import { useState, useEffect } from "react";
 import RoundButton from "./components/RoundButton";
 import InputBox from "./components/InputBox";
 import Header from "./components/Header";
-import { usePatients } from "../viewModels/learnerData";
+import InputLine from "./components/InputLine";
 
 export default function ForgotPassword({navigation}){
-    let patientsArr = usePatients(7012);
-    useEffect(()=>{
-        console.log('patientsArr: ' + patientsArr);
-    },[patientsArr])
 
     return(
         <SafeAreaView style={styles.page}>
              <View>
                  <Header></Header>
              </View>
-            <View style={styles.dialogue}>
-                <Text style={styles.titleText}>Forgot Password</Text>
-                <InputBox style = {{marginBottom: '5%'}} title='Email*'></InputBox>
-                <RoundButton onClick = {() => navigation.navigate('Onboarding')} buttonText='Continue' buttonWidth='2'></RoundButton>
-                <Text style = {{marginVertical: '3%'}}>Don't have an account?
-                    <TouchableOpacity onPress = {() => navigation.navigate('Registration')}><Text style={styles.signUpText}> Sign up</Text></TouchableOpacity>
-                </Text>
+             <View style={styles.body}>
+                <View style={styles.dialogue}>
+                    <Text style={styles.titleText}>Forgot Password</Text>
+                    <InputLine 
+                        style={{marginVertical: 80, color: '#04A69D', borderBottomColor: 'black', width: '70%'}} 
+                        placeholder='Email*'>
+                    </InputLine>
+                    <RoundButton onClick = {() => navigation.navigate('Onboarding')} buttonText='Continue' buttonWidth='2'></RoundButton>
+                    <Text style = {{marginVertical: '3%'}}>Don't have an account?
+                        <TouchableOpacity onPress = {() => navigation.navigate('Registration')}><Text style={styles.signUpText}> Sign up</Text></TouchableOpacity>
+                    </Text>
+                </View>
             </View>
-            
         </SafeAreaView>
     )
 }
@@ -33,6 +33,11 @@ const styles = StyleSheet.create({
     page: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    body: {
+        height: '60%',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     titleText: {
         marginTop: '10%',
@@ -43,10 +48,10 @@ const styles = StyleSheet.create({
     dialogue: {
         justifyContent: 'space-between',
         alignItems: 'center',
+        width: '48%',
     },
     signUpText: {
         color: '#04A69D',
         fontWeight: 'bold',
-
     }
 })
